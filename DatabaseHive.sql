22380a6a37ca6e768af3772efa631741e3261655
Daniel <dans1990@hotmail.co.uk>
2015-05-24 20:04:58 +0100

    Fixed git issues

--- DatabaseHive.sql
+++ DatabaseHive.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.15  Distrib 10.0.18-MariaDB, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.6.22, for osx10.8 (x86_64)
 --
--- Host: 127.0.0.1    Database: Hive
+-- Host: localhost    Database: Hive
 -- ------------------------------------------------------
--- Server version	10.0.18-MariaDB-log
+-- Server version	5.5.29
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -31,19 +31,10 @@ CREATE TABLE `Annotation` (
   PRIMARY KEY (`ID`),
   KEY `FileAnnoFk` (`FileId`),
   KEY `UserAnnoFk` (`UserId`),
-  CONSTRAINT `FileAnnoFk` FOREIGN KEY (`FileId`) REFERENCES `VersionInfo` (`ID`),
-  CONSTRAINT `UserAnnoFk` FOREIGN KEY (`UserId`) REFERENCES `User` (`ID`)
+  CONSTRAINT `FileAnnoFk` FOREIGN KEY (`FileId`) REFERENCES `VersionInfo` (`Id`),
+  CONSTRAINT `UserAnnoFk` FOREIGN KEY (`UserId`) REFERENCES `User` (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Annotation`
---
-
-LOCK TABLES `Annotation` WRITE;
-/*!40000 ALTER TABLE `Annotation` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Annotation` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `FileInfo`
@@ -55,20 +46,11 @@ DROP TABLE IF EXISTS `FileInfo`;
 CREATE TABLE `FileInfo` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `FileName` varchar(767) NOT NULL,
+  `FilePath` text NOT NULL,
   PRIMARY KEY (`ID`),
   UNIQUE KEY `FileNameKey` (`FileName`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `FileInfo`
---
-
-LOCK TABLES `FileInfo` WRITE;
-/*!40000 ALTER TABLE `FileInfo` DISABLE KEYS */;
-INSERT INTO `FileInfo` VALUES (1,'');
-/*!40000 ALTER TABLE `FileInfo` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `History`
@@ -84,18 +66,9 @@ CREATE TABLE `History` (
   `UserId` int(11) NOT NULL,
   PRIMARY KEY (`ID`),
   KEY `UserFk` (`UserId`),
-  CONSTRAINT `UserFk` FOREIGN KEY (`UserId`) REFERENCES `User` (`ID`)
+  CONSTRAINT `UserFk` FOREIGN KEY (`UserId`) REFERENCES `User` (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `History`
---
-
-LOCK TABLES `History` WRITE;
-/*!40000 ALTER TABLE `History` DISABLE KEYS */;
-/*!40000 ALTER TABLE `History` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `User`
@@ -125,15 +98,6 @@ CREATE TABLE `User` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `User`
---
-
-LOCK TABLES `User` WRITE;
-/*!40000 ALTER TABLE `User` DISABLE KEYS */;
-/*!40000 ALTER TABLE `User` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `VersionInfo`
 --
 
@@ -142,31 +106,28 @@ DROP TABLE IF EXISTS `VersionInfo`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `VersionInfo` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `FileInfoId` int(11) DEFAULT NULL,
-  `UserId` int(11) NOT NULL,
-  `Title` varchar(1024) DEFAULT NULL,
-  `Author` varchar(1024) DEFAULT NULL,
+  `FileName` varchar(767) DEFAULT NULL,
   `UploadDate` datetime NOT NULL,
   `CreatedDate` datetime DEFAULT NULL,
+<<<<<<< HEAD
   `CrcHash` varchar(767) DEFAULT NULL,
   `Description` longtext,
+=======
+  `Author` varchar(1024) DEFAULT NULL,
+  `FilePath` text,
+  `UserId` int(11) NOT NULL,
+  `hash` varchar(767) DEFAULT NULL,
+  `FileInfoId` int(11) DEFAULT NULL,
+>>>>>>> 6aee583... Changes for Swoorup
   PRIMARY KEY (`ID`),
-  UNIQUE KEY `HashUQ` (`CrcHash`),
+  UNIQUE KEY `FileName` (`FileName`),
+  UNIQUE KEY `HashUQ` (`hash`),
   KEY `UserFileFK` (`UserId`),
   KEY `FileInfoId` (`FileInfoId`),
-  CONSTRAINT `UserFileFK` FOREIGN KEY (`UserId`) REFERENCES `User` (`ID`),
+  CONSTRAINT `UserFileFK` FOREIGN KEY (`UserId`) REFERENCES `User` (`Id`),
   CONSTRAINT `VersionInfo_ibfk_1` FOREIGN KEY (`FileInfoId`) REFERENCES `FileInfo` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `VersionInfo`
---
-
-LOCK TABLES `VersionInfo` WRITE;
-/*!40000 ALTER TABLE `VersionInfo` DISABLE KEYS */;
-/*!40000 ALTER TABLE `VersionInfo` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `ViewHistory`
@@ -183,19 +144,10 @@ CREATE TABLE `ViewHistory` (
   PRIMARY KEY (`ID`),
   KEY `FileViewFk` (`FileId`),
   KEY `UserViewFK` (`UserId`),
-  CONSTRAINT `FileViewFk` FOREIGN KEY (`FileId`) REFERENCES `VersionInfo` (`ID`),
-  CONSTRAINT `UserViewFK` FOREIGN KEY (`UserId`) REFERENCES `User` (`ID`)
+  CONSTRAINT `FileViewFk` FOREIGN KEY (`FileId`) REFERENCES `VersionInfo` (`Id`),
+  CONSTRAINT `UserViewFK` FOREIGN KEY (`UserId`) REFERENCES `User` (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `ViewHistory`
---
-
-LOCK TABLES `ViewHistory` WRITE;
-/*!40000 ALTER TABLE `ViewHistory` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ViewHistory` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -206,4 +158,8 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
+<<<<<<< HEAD
 -- Dump completed on 2015-05-21 16:32:22
+=======
+-- Dump completed on 2015-05-24 20:04:41
+>>>>>>> 6aee583... Changes for Swoorup
